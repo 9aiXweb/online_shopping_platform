76b0891398b57112ba00aa3dc9fbe790cd8410a5
digivcam24 <vimeanseththorngstf@gmail.com>
2024-02-21 16:51:26 +0900

    seth

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # [`9aiXweb's Online Shopping Platform`]()
 
 ## URL: `<url>`
+
+
+## 機能
+- user register
+- login/logout
+- 商品の登録　(shop owner)
+
+- search product
+- payment 
+- insert product to `cart` (customer)
+- 
+
+
+## Command
+- Run
+```console
+flask --app flaskr run --port 8080
+```
+- Initialize database `DB`
+```console
+flask --app flaskr init-db
+```
---
+++ backup/_MIAN_online_shopping_failed/flaskr/__init__.py
@@ -0,0 +1,55 @@
+from flask import Flask, session, redirect, url_for, request, Blueprint, render_template
+import os
+
+
+
+def create_app(test_config=None):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "schema.sqlite"),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def index():
+        return render_template('customer/index.html')
+
+    # register the database commands
+    from . import db
+
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from . import customer
+    from . import shop_owner
+
+
+    app.register_blueprint(customer.bp)
+    app.register_blueprint(shop_owner.bp)
+
+
+    # make url_for('index') == url_for('blog.index')
+    # in another app, you might define a separate main index here with
+    # app.route, while giving the blog blueprint a url_prefix, but for
+    # the tutorial the blog will be the main index
+    app.add_url_rule("/", endpoint="index")
+
+    
+
+    return app
